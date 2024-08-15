a38d64ffd3cb8f68d32095f190277f491c0d1660
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-15 19:01:19 +0530

    refactor, extract error message component(DRY)

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm=z.infer<typeof createIssueSchema>
 const NewIssuePage = () => {
@@ -40,7 +41,9 @@ const NewIssuePage = () => {
         placeholder="Title"
         {...register("title")}
       ></TextField.Root>
-      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+      <ErrorMessage>
+        {errors.title?.message}
+      </ErrorMessage>
       <Controller
         name="description"
         control={control}
@@ -48,7 +51,9 @@ const NewIssuePage = () => {
           <SimpleMDE placeholder="Description" {...field} />
         )}
       />
-      {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+      <ErrorMessage>
+        {errors.description?.message}
+      </ErrorMessage>
       <Button>Submit the issue</Button>
     </form>
     </div>
