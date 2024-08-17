933041ba713a6caef24fd8745ffa9fc1e40dcf3f
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-17 13:44:08 +0530

    protected routes

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,16 @@
 import { createIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import authOptions from "../../auth/authOptions";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session=await getServerSession(authOptions)
+  if(!session)
+      return NextResponse.json({},{status:401})
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
@@ -29,6 +34,9 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session=await getServerSession(authOptions)
+  if(!session)
+      return NextResponse.json({},{status:401})
     const issue=await prisma.issue.findUnique({
         where:{id:parseInt(params.id)}
     });
