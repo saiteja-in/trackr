8f60cff14980ae8fe888f003e14de2b037adb93a
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-22 21:28:28 +0530

    responsive dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Flex,
+  Grid,
 } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
@@ -12,12 +13,20 @@ const page = async() => {
   const inprogress=await prisma.issue.count({where:{status:'IN_PROGRESS'}})
   const closed=await prisma.issue.count({where:{status:'CLOSED'}})
   return (
-    <Flex direction="column" gap="3">
-      <LatestIssues />
+    // <Flex direction="column" gap="3">
+    //   <LatestIssues />
+    //   <IssueSummary open={open} inProgress={inprogress} closed={closed} />
+    //   <IssueChart open={open} inProgress={inprogress} closed={closed} />
+    // </Flex>
+    <Grid columns={{initial:"1",md:"2"}} gap="5">
+      <Flex direction="column" gap="5">
       <IssueSummary open={open} inProgress={inprogress} closed={closed} />
       <IssueChart open={open} inProgress={inprogress} closed={closed} />
-    </Flex>
+
+      </Flex>
+      <LatestIssues />
+    </Grid>
   );
 };
-
+ 
 export default page;
