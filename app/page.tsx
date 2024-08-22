bf9f8f3a8dfb0d2a451f3fb08e81609ac5da014e
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-22 21:10:52 +0530

    build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,19 @@
 import React from "react";
-import Pagination from "./components/Pagination";
-import { AlertDialog, Avatar, Badge, Blockquote, Box, Button, Checkbox, Code, Flex, IconButton, Popover, Text, TextArea } from "@radix-ui/themes";
-import { ChatBubbleIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import {
+  Flex,
+} from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-const page = ({ searchParams }: { searchParams: { page: string } }) => {
+const page = async() => {
+  const open=await prisma.issue.count({where:{status:'OPEN'}})
+  const inprogress=await prisma.issue.count({where:{status:'IN_PROGRESS'}})
+  const closed=await prisma.issue.count({where:{status:'CLOSED'}})
   return (
     <Flex direction="column" gap="3">
-      <Text size="5">The quick brown fox jumps over the lazy dog.</Text>
-      <Code>console.log("Hello Nextjs")</Code>
-      <Blockquote>
-        Perfect typography is certainly the most elusive of all arts. Sculpture
-        in stone alone comes near it in obstinacy.
-      </Blockquote>
- 
-
-<Flex gap="2">
-  <Badge color="orange">In progress</Badge>
-  <Badge color="blue">In review</Badge>
-  <Badge color="green">Complete</Badge>
-</Flex>
-<IconButton>
-  <MagnifyingGlassIcon width="18" height="18" />
-</IconButton>
-<Popover.Root>
-  <Popover.Trigger>
-    <Button variant="soft">
-      <ChatBubbleIcon width="16" height="16" />
-      Comment
-    </Button>
-  </Popover.Trigger>
-  <Popover.Content width="360px">
-    <Flex gap="3">
-      <Avatar
-        size="2"
-        src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-        fallback="A"
-        radius="full"
-      />
-      <Box flexGrow="1">
-        <TextArea placeholder="Write a comment…" style={{ height: 80 }} />
-        <Flex gap="3" mt="3" justify="between">
-          <Flex align="center" gap="2" asChild>
-            <Text as="label" size="2">
-              <Checkbox />
-              <Text>Send to group</Text>
-            </Text>
-          </Flex>
-
-          <Popover.Close>
-            <Button size="1">Comment</Button>
-          </Popover.Close>
-        </Flex>
-      </Box>
-    </Flex>
-  </Popover.Content>
-</Popover.Root>
-
-<LatestIssues />
-<IssueSummary open={10} inProgress={5} closed={6} />
+      <LatestIssues />
+      <IssueSummary open={open} inProgress={inprogress} closed={closed} />
     </Flex>
   );
 };
